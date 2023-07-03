ffbf5037fd9f6d73336f9cf1f36fc70bbbcc48d2
Alone <tranluong460@gmail.com>
2023-07-03 21:54:17 +0700

    feat: up router refs: PLS-2

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BaseClient from "./pages/client/BaseClient";
 import HomePage from "./pages/client/Home/HomePage";
+import IntroducePage from "./pages/client/Introduce/IntroducePage";
+import ContactPage from "./pages/client/Contact/ContactPage";
+import ListProductPage from "./pages/client/ListProduct/ListProductPage";
+import CartPage from "./pages/client/Cart/CartPage";
 
 import BaseAuth from "./pages/auth/BaseAuth";
 import LoginPage from "./pages/auth/Login/LoginPage";
 import RegisterPage from "./pages/auth/Register/RegisterPage";
-import IntroducePage from "./pages/client/Introduce/IntroducePage";
-import ContactPage from "./pages/client/Contact/ContactPage";
-import ListProductPage from "./pages/client/ListProduct/ListProductPage";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             <Route path="introduce" element={<IntroducePage />} />
             <Route path="contact" element={<ContactPage />} />
             <Route path="list-product" element={<ListProductPage />} />
+            <Route path="cart" element={<CartPage />} />
           </Route>
 
           <Route path="/auth" element={<BaseAuth />}>
