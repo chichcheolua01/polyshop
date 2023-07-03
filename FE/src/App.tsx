d383d1ed83462f8054f3c2b03d35f372fc36d0d5
Alone <tranluong460@gmail.com>
2023-07-03 21:52:45 +0700

    feat: up router refs: PLS-2

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -5,6 +5,9 @@ import HomePage from "./pages/client/Home/HomePage";
 import BaseAuth from "./pages/auth/BaseAuth";
 import LoginPage from "./pages/auth/Login/LoginPage";
 import RegisterPage from "./pages/auth/Register/RegisterPage";
+import IntroducePage from "./pages/client/Introduce/IntroducePage";
+import ContactPage from "./pages/client/Contact/ContactPage";
+import ListProductPage from "./pages/client/ListProduct/ListProductPage";
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
         <Routes>
           <Route path="/" element={<BaseClient />}>
             <Route index element={<HomePage />} />
+            <Route path="introduce" element={<IntroducePage />} />
+            <Route path="contact" element={<ContactPage />} />
+            <Route path="list-product" element={<ListProductPage />} />
           </Route>
 
           <Route path="/auth" element={<BaseAuth />}>
