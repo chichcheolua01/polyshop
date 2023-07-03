293a606507bdfabba6c55a139e537a73ab5133cf
Alone <tranluong460@gmail.com>
2023-07-03 12:43:14 +0700

    feat: up home page refs: PLS-1

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import BaseClient from "./pages/client/BaseClient";
+import HomePage from "./pages/client/Home/HomePage";
+
 function App() {
   return (
     <>
-      <div>Home</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<BaseClient />}>
+            <Route index element={<HomePage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ FE/src/pages/client/BaseClient.tsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router-dom";
+
+const BaseClient = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
+
+export default BaseClient;
