07e5772d04ec8fafb942c97b2b112d1cb2096599
phucquyet1202 <phucquyet1222003@gmail.com>
2023-08-06 23:18:46 +0700

    feat:[FE] cart refs: PLS-48

--- FE/src/store/index.ts
+++ FE/src/store/index.ts
@@ -1,9 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
 import { productApi } from "../api/products";
 import { categoryApi } from "../api/categories";
 import { authApi } from "../api/auth";
 
+// Cấu hình persist ( lưu localStorage )
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
 export const store = configureStore({
   reducer: {
     [productApi.reducerPath]: productApi.reducer,
