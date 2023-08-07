4419345ff59c53be06c306f191ef9f5f6cceb950
Alone <tranluong460@gmail.com>
2023-08-07 18:50:53 +0700

    feat: up API contact refs: PLS-42

--- FE/src/store/index.ts
+++ FE/src/store/index.ts
@@ -3,16 +3,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import { productApi } from "../api/products";
 import { categoryApi } from "../api/categories";
 import { authApi } from "../api/auth";
+import { contactApi } from "../api/contact";
 
 export const store = configureStore({
   reducer: {
     [productApi.reducerPath]: productApi.reducer,
     [categoryApi.reducerPath]: categoryApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
+    [contactApi.reducerPath]: contactApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(productApi.middleware)
       .concat(categoryApi.middleware)
-      .concat(authApi.middleware),
+      .concat(authApi.middleware)
+      .concat(contactApi.middleware),
 });
